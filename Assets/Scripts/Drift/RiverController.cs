427bd183abcf2eea6e6f3b0bdee2698997b882ec
fxd <mygoddess007700@gmail.com>
2022-10-20 20:38:34 +0800

    ✨ feat: 用于控制水平面变化

---
+++ Assets/Scripts/Drift/RiverController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RiverController : MonoBehaviour
+{
+    public Transform boat;
+    public BuoyancyEffector2D effector2D;
+    public float[] level;
+
+    public int levelIndex = 0;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Up"))
+        {
+            if (levelIndex > 0)
+            {
+                levelIndex--;
+            }
+        }
+        else if (Input.GetButtonDown("Down"))
+        {
+            if (levelIndex < level.Length - 1)
+            {
+                levelIndex++;
+            }
+        }
+        effector2D.surfaceLevel = level[levelIndex];
+    }
+}
