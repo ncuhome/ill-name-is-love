cbba8a8482a59c43e023f27fcfff1dbcef02cc80
fxd <mygoddess007700@gmail.com>
2022-10-26 02:19:33 +0800

    ✨ feat: 打包原型的暂时设置

--- Assets/Scripts/Begin/BeginControl.cs
+++ Assets/Scripts/Begin/BeginControl.cs
@@ -7,6 +7,31 @@ public class BeginControl : MonoBehaviour
 {
     void Awake()
     {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnClick1()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnClick2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void OnClick3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void OnClick4()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void OnClick5()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 5);
     }
 }
